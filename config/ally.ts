29414188f627739c6e36eb659864b4a31586b95d
josiashod <djossoujosiashod@gmail.com>
2022-08-18 13:20:45 +0100

    update callback google system

--- config/ally.ts
+++ config/ally.ts
@@ -27,7 +27,8 @@ const allyConfig: AllyConfig = {
     driver: 'google',
     clientId: Env.get('GOOGLE_CLIENT_ID'),
     clientSecret: Env.get('GOOGLE_CLIENT_SECRET'),
-    callbackUrl: 'http://localhost:3333/api/v1/auth/google/callback',
+    callbackUrl: Env.get('GOOGLE_CALLBACK_URL'),
+    scopes: ['userinfo.email', 'userinfo.profile'],
   },
 }
 
