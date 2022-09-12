9274961f3f49b32dbb7d02ab05f262d1bb815f83
josiashod <djossoujosiashod@gmail.com>
2022-09-12 11:03:08 +0100

    Update email issues fixed

--- app/Actions/Auth/EmailConfirmationProfileAction.ts
+++ app/Actions/Auth/EmailConfirmationProfileAction.ts
@@ -1,41 +1,39 @@
-import HttpException from "App/Exceptions/HttpException"
-import User from "App/Models/User"
-import { $t } from "App/Services/i18n"
-import Tokenizer from "App/Services/Tokenizer"
+import HttpException from 'App/Exceptions/HttpException'
+import User from 'App/Models/User'
+import { $t } from 'App/Services/i18n'
+import Tokenizer from 'App/Services/Tokenizer'
 import Config from '@ioc:Adonis/Core/Config'
-import { DateTime } from "luxon"
+import { DateTime } from 'luxon'
 
 export default class EmailConfirmationProfileAction {
-    public async handle(token, email) {
-        
-        const user = await User.query().where('email', email).firstOrFail()
+  public async handle(token, email) {
+    const user = await User.query().where('email', email).firstOrFail()
 
-        if (user.confirmedAt) {
-          throw new HttpException($t('auth.email_already_confirmed'), 400)
-        }
-    
-        if (!user.confirmationSentAt || !user.confirmationDigest) {
-          throw new HttpException($t('auth.token_invalid'), 400)
-        }
-    
-        const timeOut = user.confirmationSentAt.diffNow('seconds').seconds
-    
-        if (timeOut > Config.get('timeout.confirmationToken')) {
-          throw new HttpException($t('auth.token_expired'), 400)
-        }
-    
-        if (!(await Tokenizer.verify(user.confirmationDigest, token))) {
-          throw new HttpException($t('auth.token_invalid'), 400)
-        }
-    
-        user.merge({
-          confirmationDigest: null,
-          confirmationSentAt: null,
-          confirmedAt: DateTime.utc(),
-        })
-        user.save()
+    if (user.confirmedAt) {
+      throw new HttpException($t('auth.email_already_confirmed'), 400)
+    }
 
-        return true
-      }
+    if (!user.confirmationSentAt || !user.confirmationDigest) {
+      throw new HttpException($t('auth.token_invalid'), 400)
+    }
+
+    const timeOut = user.confirmationSentAt.diffNow('seconds').seconds
+
+    if (timeOut > Config.get('timeout.confirmationToken')) {
+      throw new HttpException($t('auth.token_expired'), 400)
+    }
+
+    if (!(await Tokenizer.verify(user.confirmationDigest, token))) {
+      throw new HttpException($t('auth.token_invalid'), 400)
+    }
+
+    user.merge({
+      confirmationDigest: null,
+      confirmationSentAt: null,
+      confirmedAt: DateTime.utc(),
+    })
+    user.save()
+
+    return true
   }
-  
+}
